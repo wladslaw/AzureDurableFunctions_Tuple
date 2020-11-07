5a405be99cb5992a48309877bcab641552a36755
Vladislav Shirshakov <vshirshakov@mentalstack.com>
2020-11-07 16:11:38 +0300

    Initial commit

---
+++ Company.FunctionApp/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Company.FunctionApp
+{
+    public static class Test
+    {
+        [FunctionName("Test")]
+        public static Task<string> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var input = context.GetInput<Input>();
+            return Task.FromResult(input.Item2);
+        }
+
+        [FunctionName("Test_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]
+            HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            var instanceId = await starter.StartNewAsync("Test", new Input(1, "2", Guid.NewGuid()));
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+
+    public class Input : Tuple<int, string, Guid>
+    {
+        public Input(int item1, string item2, Guid item3) : base(item1, item2, item3)
+        {
+        }
+    }
+}
